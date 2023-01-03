df9efcccb3e7143a3d023a24c1afe43d3680ecc4
Anirudh Ganti <anirudhganti@gmail.com>
2023-01-03 23:53:38 +1100

    Remote

--- GCloud_Connection_V5_Test.py
+++ GCloud_Connection_V5_Test.py
@@ -1,20 +1,20 @@
-#### Import Python libraries ####
+# Import Python libraries
 import sqlite3
 from google.cloud import storage
 import pandas as pd
 import numpy as np
 from datetime import datetime
-import mysql.connector
+# import mysql.connector
 import sys
 import pg8000
 from google.cloud.sql.connector import Connector, IPTypes
 import os
 import sqlalchemy
-# ----------------------------------------------------------------------------------------------------------------------
-#from google.colab import auth
-#auth.authenticate_user()
 
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "X:\Limitless\A - Skeletal Tracking\Keys\service_key_gcloud.json"
+# ----------------------------------------------------------------------------------------------------------------------
+# AUTHENTICATION
+# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "X:\Limitless\A - Skeletal Tracking\Keys\service_key_gcloud.json"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "F:\Limitless\Programs\Keys\service_key_gcloud.json"
 
 INSTANCE_CONNECTION_NAME = f"applied-craft-372501:australia-southeast2:imikami-demo-v1"
 print(f"Your instance connection name is: {INSTANCE_CONNECTION_NAME}")
@@ -24,6 +24,8 @@ DB_NAME = "postgres"
 # ----------------------------------------------------------------------------------------------------------------------
 # initialize Connector object
 connector = Connector()
+
+
 # function to return the database connection object
 def getconn():
     conn = connector.connect(
@@ -32,9 +34,10 @@ def getconn():
         user=DB_USER,
         password=DB_PASS,
         db=DB_NAME,
-        enable_iam_auth = True
+        enable_iam_auth=True
     )
     return conn
+
 
 # create connection pool with 'creator' argument to our connection object function
 pool = sqlalchemy.create_engine(
@@ -44,28 +47,28 @@ pool = sqlalchemy.create_engine(
 
 # connect to connection pool
 with pool.connect() as db_conn:
-  # create ratings table in our movies database
-  db_conn.execute(
-      "CREATE TABLE IF NOT EXISTS Movies "
-      "( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, "
-      "genre VARCHAR(255) NOT NULL, rating FLOAT NOT NULL, "
-      "PRIMARY KEY (id));"
-  )
-  # insert data into our ratings table
-  insert_stmt = sqlalchemy.text(
-      "INSERT INTO ratings (title, genre, rating) VALUES (:title, :genre, :rating)",
-  )
+    # create ratings table in our movies database
+    db_conn.execute(
+        "CREATE TABLE IF NOT EXISTS Movies "
+        "( id SERIAL NOT NULL, title VARCHAR(255) NOT NULL, "
+        "genre VARCHAR(255) NOT NULL, rating FLOAT NOT NULL, "
+        "PRIMARY KEY (id));"
+    )
+    # insert data into our ratings table
+    insert_stmt = sqlalchemy.text(
+        "INSERT INTO ratings (title, genre, rating) VALUES (:title, :genre, :rating)",
+    )
 
-  # insert entries into table
-  db_conn.execute(insert_stmt, title="Batman Begins", genre="Action", rating=8.5)
-  db_conn.execute(insert_stmt, title="Star Wars: Return of the Jedi", genre="Action", rating=9.1)
-  db_conn.execute(insert_stmt, title="The Breakfast Club", genre="Drama", rating=8.3)
+    # insert entries into table
+    db_conn.execute(insert_stmt, title="Batman Begins", genre="Action", rating=8.5)
+    db_conn.execute(insert_stmt, title="Star Wars: Return of the Jedi", genre="Action", rating=9.1)
+    db_conn.execute(insert_stmt, title="The Breakfast Club", genre="Drama", rating=8.3)
 
-  # query and fetch ratings table
-  results = db_conn.execute("SELECT * FROM ratings").fetchall()
+    # query and fetch ratings table
+    results = db_conn.execute("SELECT * FROM ratings").fetchall()
 
-  # show results
-  for row in results:
-    print(row)
+    # show results
+    for row in results:
+        print(row)
 
 connector.close()
